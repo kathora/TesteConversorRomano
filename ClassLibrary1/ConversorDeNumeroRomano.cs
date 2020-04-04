ce7635d84a3f7a7da4fbca7d0182c8531b58d09a
Kat Hora <kathorahora@gmail.com>
2020-04-04 16:40:03 +0100

    First Test

---
+++ ClassLibrary1/ConversorDeNumeroRomano.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorNumeroRomano
+{
+    public class ConversorDeNumeroRomano
+    {
+        public string Converte(int numeroEmRomano) 
+        {
+            if (numeroEmRomano.Equals(1))
+                return "I";
+            else if (numeroEmRomano.Equals(5))
+                return "V";
+            return "0";
+        }
+    }
+}
---
+++ ClassLibrary2/ConversorDeNumeroRomanoTest.cs
@@ -0,0 +1,29 @@
+﻿using ConversorNumeroRomano;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace ClassLibrary2
+{
+    [ExcludeFromCodeCoverage]
+    public class ConversorDeNumeroRomanoTest
+    {
+        [Fact]
+        public void DeveEntenderOSimboloI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            string numero = romano.Converte(1);
+            Assert.Equal("I", numero);
+
+        }
+
+        [Fact]
+        public void DeveEntenderOSimboloV()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            string numero = romano.Converte(5);
+            Assert.Equal("V", numero);
+
+        }
+    }
+}
